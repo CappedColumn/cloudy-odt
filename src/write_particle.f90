93f2c276d96447325c82b11d4cc3dcb23f1b71d1
Corey Bois <corey.bois@utah.edu>
2026-03-20 14:10:38 -0600

    Add create_particle_netcdf subroutine
    Defines the _particles.nc file structure using CF contiguous ragged
    array convention: two unlimited dimensions (record, time_step) with
    per-record particle fields and per-time-step time/row_sizes variables.
    All variables use zlib compression. No calls wired up yet.

--- src/write_particle.f90
+++ src/write_particle.f90
@@ -1,5 +1,6 @@
 module write_particle
     use, intrinsic :: iso_c_binding
+    use netcdf
     use droplets, only: particle, particles, current_n_particles, write_trajectories, trajectory_start, trajectory_end, write_particle_id, trajectory_timer
     use globals
     use writeout, only: nc_verify
@@ -25,10 +26,15 @@ module write_particle
 
     ! file unit  handling
     integer :: data_unit, meta_unit
-    
+
     ! tracking state management
     logical :: tracking_activated = .false.
     real(dp) :: trajectory_time_iter = 0.
+
+    ! NetCDF particle output state
+    integer :: pnc_id
+    integer :: record_count = 0
+    integer :: time_step_count = 0
 
 
     ! Main - write_trajectory_controller
@@ -152,7 +158,7 @@ module write_particle
 
 
     subroutine wparticle_initialize(this, droplet)
-        ! Copies over properties of particle type into the 
+        ! Copies over properties of particle type into the
         ! write-out version
         type(w_particle), intent(out) :: this
         type(particle), intent(in) :: droplet
@@ -167,6 +173,95 @@ module write_particle
         this%activated = droplet%activated
 
     end subroutine wparticle_initialize
+
+    subroutine create_particle_netcdf(filename)
+        ! Creates the _particles.nc file using CF contiguous ragged array convention.
+        ! Defines dimensions (record, time_step) and all per-record / per-time-step
+        ! variables with compression and CF attributes.
+        character(*), intent(in) :: filename
+
+        integer :: rec_dimid, ts_dimid
+        integer :: varid
+
+        ! Reset counters
+        record_count = 0
+        time_step_count = 0
+
+        ! Create file
+        call nc_verify( nf90_create(trim(filename)//'_particles.nc', NF90_NETCDF4, pnc_id), &
+                         "create_particle_netcdf: nf90_create" )
+
+        ! Dimensions (both unlimited)
+        call nc_verify( nf90_def_dim(pnc_id, "record", NF90_UNLIMITED, rec_dimid) )
+        call nc_verify( nf90_def_dim(pnc_id, "time_step", NF90_UNLIMITED, ts_dimid) )
+
+        ! --- Per-record variables (on record dimension) ---
+
+        call nc_verify( nf90_def_var(pnc_id, "particle_id", NF90_INT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Particle ID") )
+
+        call nc_verify( nf90_def_var(pnc_id, "aerosol_id", NF90_INT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Aerosol Type ID") )
+
+        call nc_verify( nf90_def_var(pnc_id, "gridcell", NF90_INT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Grid Cell Index") )
+
+        call nc_verify( nf90_def_var(pnc_id, "position", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Vertical Position") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "m") )
+
+        call nc_verify( nf90_def_var(pnc_id, "temperature", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Temperature") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "K") )
+
+        call nc_verify( nf90_def_var(pnc_id, "water_vapor", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Water Vapor Mixing Ratio") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "kg/kg") )
+
+        call nc_verify( nf90_def_var(pnc_id, "supersaturation", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Supersaturation") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "%") )
+
+        call nc_verify( nf90_def_var(pnc_id, "radius", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Droplet Radius") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "um") )
+
+        call nc_verify( nf90_def_var(pnc_id, "solute_radius", NF90_FLOAT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Dry Solute Radius") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "m") )
+
+        call nc_verify( nf90_def_var(pnc_id, "activated", NF90_INT, rec_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Activation Flag") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "flag_values", (/0, 1/)) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "flag_meanings", "unactivated activated") )
+
+        ! --- Per-time-step variables (on time_step dimension) ---
+
+        call nc_verify( nf90_def_var(pnc_id, "time", NF90_DOUBLE, ts_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Time") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "units", "seconds") )
+
+        call nc_verify( nf90_def_var(pnc_id, "row_sizes", NF90_INT, ts_dimid, varid, &
+                         deflate_level=1, shuffle=.true.) )
+        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Number of particles per time step") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "cf_role", "ragged_row_sizes") )
+        call nc_verify( nf90_put_att(pnc_id, varid, "sample_dimension", "record") )
+
+        ! Exit define mode
+        call nc_verify( nf90_enddef(pnc_id), "create_particle_netcdf: nf90_enddef" )
+
+    end subroutine create_particle_netcdf
 
 
 end module write_particle

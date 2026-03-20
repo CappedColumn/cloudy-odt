b92c6c3dc83466b4a2d79ff792ea48f4b7d529b9
Corey Bois <corey.bois@utah.edu>
2026-03-20 16:49:17 -0600

    Cache NetCDF variable IDs in particle writer
    Store varid values as module-level variables during file creation
    instead of calling nf90_inq_varid on every write. Eliminates 12
    redundant lookups per trajectory time step.

--- src/write_particle.f90
+++ src/write_particle.f90
@@ -14,6 +14,12 @@ module write_particle
     integer :: record_count = 0
     integer :: time_step_count = 0
 
+    ! Cached NetCDF variable IDs (set once in create_particle_netcdf)
+    integer :: vid_particle_id, vid_aerosol_id, vid_gridcell, vid_position
+    integer :: vid_temperature, vid_water_vapor, vid_supersaturation, vid_radius
+    integer :: vid_solute_radius, vid_activated
+    integer :: vid_time, vid_row_sizes
+
 contains
 
     subroutine write_trajectory_controller(lparticles, ltime, ldt)
@@ -52,7 +58,6 @@ contains
         character(*), intent(in) :: filename
 
         integer :: rec_dimid, ts_dimid
-        integer :: varid
 
         ! Reset counters
         record_count = 0
@@ -68,66 +73,66 @@ contains
 
         ! --- Per-record variables (on record dimension) ---
 
-        call nc_verify( nf90_def_var(pnc_id, "particle_id", NF90_INT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "particle_id", NF90_INT, rec_dimid, vid_particle_id, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Particle ID") )
+        call nc_verify( nf90_put_att(pnc_id, vid_particle_id, "long_name", "Particle ID") )
 
-        call nc_verify( nf90_def_var(pnc_id, "aerosol_id", NF90_INT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "aerosol_id", NF90_INT, rec_dimid, vid_aerosol_id, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Aerosol Type ID") )
+        call nc_verify( nf90_put_att(pnc_id, vid_aerosol_id, "long_name", "Aerosol Type ID") )
 
-        call nc_verify( nf90_def_var(pnc_id, "gridcell", NF90_INT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "gridcell", NF90_INT, rec_dimid, vid_gridcell, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Grid Cell Index") )
+        call nc_verify( nf90_put_att(pnc_id, vid_gridcell, "long_name", "Grid Cell Index") )
 
-        call nc_verify( nf90_def_var(pnc_id, "position", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "position", NF90_FLOAT, rec_dimid, vid_position, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Vertical Position") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "m") )
+        call nc_verify( nf90_put_att(pnc_id, vid_position, "long_name", "Vertical Position") )
+        call nc_verify( nf90_put_att(pnc_id, vid_position, "units", "m") )
 
-        call nc_verify( nf90_def_var(pnc_id, "temperature", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "temperature", NF90_FLOAT, rec_dimid, vid_temperature, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Temperature") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "celsius") )
+        call nc_verify( nf90_put_att(pnc_id, vid_temperature, "long_name", "Temperature") )
+        call nc_verify( nf90_put_att(pnc_id, vid_temperature, "units", "celsius") )
 
-        call nc_verify( nf90_def_var(pnc_id, "water_vapor", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "water_vapor", NF90_FLOAT, rec_dimid, vid_water_vapor, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Water Vapor Mixing Ratio") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "g/kg") )
+        call nc_verify( nf90_put_att(pnc_id, vid_water_vapor, "long_name", "Water Vapor Mixing Ratio") )
+        call nc_verify( nf90_put_att(pnc_id, vid_water_vapor, "units", "g/kg") )
 
-        call nc_verify( nf90_def_var(pnc_id, "supersaturation", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "supersaturation", NF90_FLOAT, rec_dimid, vid_supersaturation, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Supersaturation") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "%") )
+        call nc_verify( nf90_put_att(pnc_id, vid_supersaturation, "long_name", "Supersaturation") )
+        call nc_verify( nf90_put_att(pnc_id, vid_supersaturation, "units", "%") )
 
-        call nc_verify( nf90_def_var(pnc_id, "radius", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "radius", NF90_FLOAT, rec_dimid, vid_radius, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Droplet Radius") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "um") )
+        call nc_verify( nf90_put_att(pnc_id, vid_radius, "long_name", "Droplet Radius") )
+        call nc_verify( nf90_put_att(pnc_id, vid_radius, "units", "um") )
 
-        call nc_verify( nf90_def_var(pnc_id, "solute_radius", NF90_FLOAT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "solute_radius", NF90_FLOAT, rec_dimid, vid_solute_radius, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Dry Solute Radius") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "m") )
+        call nc_verify( nf90_put_att(pnc_id, vid_solute_radius, "long_name", "Dry Solute Radius") )
+        call nc_verify( nf90_put_att(pnc_id, vid_solute_radius, "units", "m") )
 
-        call nc_verify( nf90_def_var(pnc_id, "activated", NF90_INT, rec_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "activated", NF90_INT, rec_dimid, vid_activated, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Activation Flag") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "flag_values", (/0, 1/)) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "flag_meanings", "unactivated activated") )
+        call nc_verify( nf90_put_att(pnc_id, vid_activated, "long_name", "Activation Flag") )
+        call nc_verify( nf90_put_att(pnc_id, vid_activated, "flag_values", (/0, 1/)) )
+        call nc_verify( nf90_put_att(pnc_id, vid_activated, "flag_meanings", "unactivated activated") )
 
         ! --- Per-time-step variables (on time_step dimension) ---
 
-        call nc_verify( nf90_def_var(pnc_id, "time", NF90_DOUBLE, ts_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "time", NF90_DOUBLE, ts_dimid, vid_time, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Time") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "units", "seconds") )
+        call nc_verify( nf90_put_att(pnc_id, vid_time, "long_name", "Time") )
+        call nc_verify( nf90_put_att(pnc_id, vid_time, "units", "seconds") )
 
-        call nc_verify( nf90_def_var(pnc_id, "row_sizes", NF90_INT, ts_dimid, varid, &
+        call nc_verify( nf90_def_var(pnc_id, "row_sizes", NF90_INT, ts_dimid, vid_row_sizes, &
                          deflate_level=1, shuffle=.true.) )
-        call nc_verify( nf90_put_att(pnc_id, varid, "long_name", "Number of particles per time step") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "cf_role", "ragged_row_sizes") )
-        call nc_verify( nf90_put_att(pnc_id, varid, "sample_dimension", "record") )
+        call nc_verify( nf90_put_att(pnc_id, vid_row_sizes, "long_name", "Number of particles per time step") )
+        call nc_verify( nf90_put_att(pnc_id, vid_row_sizes, "cf_role", "ragged_row_sizes") )
+        call nc_verify( nf90_put_att(pnc_id, vid_row_sizes, "sample_dimension", "record") )
 
         ! Exit define mode
         call nc_verify( nf90_enddef(pnc_id), "create_particle_netcdf: nf90_enddef" )
@@ -141,7 +146,7 @@ contains
         type(particle), intent(in) :: lparticles(:)
         real(dp), intent(in) :: ltime
 
-        integer :: np, i, varid
+        integer :: np, i
         integer(i4), allocatable :: int_buf(:)
         real, allocatable :: float_buf(:)
 
@@ -151,59 +156,47 @@ contains
         ! --- Per-time-step variables ---
         time_step_count = time_step_count + 1
 
-        call nc_verify( nf90_inq_varid(pnc_id, "time", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, ltime, start=(/time_step_count/)) )
-
-        call nc_verify( nf90_inq_varid(pnc_id, "row_sizes", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, np, start=(/time_step_count/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_time, ltime, start=(/time_step_count/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_row_sizes, np, start=(/time_step_count/)) )
 
         ! --- Per-record variables ---
         allocate(int_buf(np), float_buf(np))
 
         ! particle_id
         do i = 1, np; int_buf(i) = lparticles(i)%particle_id; end do
-        call nc_verify( nf90_inq_varid(pnc_id, "particle_id", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, int_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_particle_id, int_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! aerosol_id
         do i = 1, np; int_buf(i) = lparticles(i)%solute_type%aerosol_id; end do
-        call nc_verify( nf90_inq_varid(pnc_id, "aerosol_id", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, int_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_aerosol_id, int_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! gridcell
         do i = 1, np; int_buf(i) = lparticles(i)%gridcell; end do
-        call nc_verify( nf90_inq_varid(pnc_id, "gridcell", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, int_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_gridcell, int_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! position
         do i = 1, np; float_buf(i) = real(lparticles(i)%position); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "position", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_position, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! temperature (K -> C)
         do i = 1, np; float_buf(i) = real(lparticles(i)%temperature - Tice); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "temperature", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_temperature, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! water_vapor (kg/kg -> g/kg)
         do i = 1, np; float_buf(i) = real(lparticles(i)%water_vapor * g_per_kg); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "water_vapor", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_water_vapor, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! supersaturation
         do i = 1, np; float_buf(i) = real(lparticles(i)%supersaturation); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "supersaturation", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_supersaturation, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! radius (m -> um)
         do i = 1, np; float_buf(i) = real(lparticles(i)%radius * um_per_m); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "radius", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_radius, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! solute_radius
         do i = 1, np; float_buf(i) = real(lparticles(i)%solute_radius); end do
-        call nc_verify( nf90_inq_varid(pnc_id, "solute_radius", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, float_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_solute_radius, float_buf, start=(/record_count+1/), count=(/np/)) )
 
         ! activated (logical -> 0/1)
         do i = 1, np
@@ -213,8 +206,7 @@ contains
                 int_buf(i) = 0
             end if
         end do
-        call nc_verify( nf90_inq_varid(pnc_id, "activated", varid) )
-        call nc_verify( nf90_put_var(pnc_id, varid, int_buf, start=(/record_count+1/), count=(/np/)) )
+        call nc_verify( nf90_put_var(pnc_id, vid_activated, int_buf, start=(/record_count+1/), count=(/np/)) )
 
         deallocate(int_buf, float_buf)
 
